100b7d714765e364be8d2f2526d5ac8d43776a21
varmaun9 <varma.1792gmail.com>
2019-09-15 13:30:01 +0100

    Hotels application
    Spring Security and Spring JWT/OAuth2

---
+++ Hotels_App/src/main/java/com/hotels/kafkasample/HotelsAppApplication.java
@@ -0,0 +1,13 @@
+package com.hotels.kafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsAppApplication.class, args);
+	}
+
+}
---
+++ Hotels_App/src/main/java/com/hotels/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.hotels.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	 
+	    @Override
+	    protected void configure(AuthenticationManagerBuilder auth)
+	      throws Exception {
+	        auth
+	          .inMemoryAuthentication()
+	          .withUser("user")
+	            .password("password")
+	            .roles("USER")
+	            .and()
+	          .withUser("admin")
+	            .password("admin")
+	            .roles("USER", "ADMIN");
+	    }
+	 
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	          .authorizeRequests()
+	          .anyRequest()
+	          .authenticated()
+	          .and()
+	          .httpBasic()
+	          .and()
+	            .logout().deleteCookies("JSESSIONID")
+	          .and()  
+	          .csrf()
+	          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	    }
+	    
+	    
+	}
+
